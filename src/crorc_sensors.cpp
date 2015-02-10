b614b7c68b387f966aa7bd635201d410c91b10a3
Heiko Engel <hengel@cern.ch>
2015-02-10 14:46:11 +0100

    consistent GPLv3 licensing

--- src/crorc_sensors.cpp
+++ src/crorc_sensors.cpp
@@ -1,22 +1,20 @@
 /**
- * @file crorc_sensors.cpp
- * @author Heiko Engel <hengel@cern.ch>
- * @version 0.1
- * @date 2014-08-25
+ *  crorc_sensors.cpp
+ *  Copyright (C) 2015 Heiko Engel <hengel@cern.ch>
  *
- * @section LICENSE
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or 
+ *  (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details at
- * http://www.gnu.org/copyleft/gpl.html
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
  *
- * */
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ **/
 
 #include <getopt.h>
 #include <string>
