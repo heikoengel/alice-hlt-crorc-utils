4bcd47f4c07f2a797ec20301470af77d49c3380a
Heiko Engel <hengel@cern.ch>
2016-04-12 14:14:47 +0200

    crorc_hwcf_coproc_zmq: proper cleanup, check FCF error flags

--- src/crorc_hwcf_coproc_zmq.cpp
+++ src/crorc_hwcf_coproc_zmq.cpp
@@ -31,8 +31,8 @@
   "usage: crorc_hwcf_coproc [parameters]\n"                                    \
   "    -n [Id]          Device ID, default:0\n"                                \
   "    -c [Id]          Channel ID, default:0\n"                               \
-  "    -p [tpcPatch]    TPC Patch ID, default:0\n"                               \
-  "    -r [rcuVersion]  TPC RCU version, default:1\n"                               \
+  "    -p [tpcPatch]    TPC Patch ID, default:0\n"                             \
+  "    -r [rcuVersion]  TPC RCU version, default:1\n"                          \
   "    -m [mappingfile] Path to AliRoot TPC Row Mapping File\n"
 
 #define ES2HOST_EB_ID 0
@@ -55,8 +55,10 @@ int eventToDisk(const char *filename, librorc::EventDescriptor *report,
                 const uint32_t *event);
 int compareToRef(const char *filename, librorc::EventDescriptor *report,
                  const uint32_t *event);
-void cleanup(crorc_hwcf_coproc_handler *stream, librorc::bar *bar,
-             librorc::device *dev, void *zmq_ctx, void *zmq_skt);
+void cleanup(crorc_hwcf_coproc_handler **stream, librorc::bar **bar,
+             librorc::device **dev, void **zmq_ctx, void **zmq_skt);
+void checkHwcfFlags(librorc::EventDescriptor *report,
+                    const char *outputFileName);
 
 inline long long timediff_us(struct timeval from, struct timeval to) {
   return ((long long)(to.tv_sec - from.tv_sec) * 1000000LL +
@@ -108,9 +110,9 @@ int main(int argc, char *argv[]) {
   }
 
   if (!mappingfile) {
-      cerr << "ERROR: no FCF mapping file provided!" << endl;
-      cout << HELP_TEXT;
-      return -1;
+    cerr << "ERROR: no FCF mapping file provided!" << endl;
+    cout << HELP_TEXT;
+    return -1;
   }
 
   crorc_hwcf_coproc_handler *stream = NULL;
@@ -123,7 +125,7 @@ int main(int argc, char *argv[]) {
   zmq_skt = zmq_socket(zmq_ctx, ZMQ_PULL);
   if (!zmq_skt) {
     perror("zmq_skt");
-    cleanup(stream, bar, dev, zmq_ctx, zmq_skt);
+    cleanup(&stream, &bar, &dev, &zmq_ctx, &zmq_skt);
     return -1;
   }
 
@@ -131,9 +133,9 @@ int main(int argc, char *argv[]) {
   char zmq_bind_addr[1024];
   snprintf(zmq_bind_addr, 1024, "tcp://*:%d", zmq_port);
   if (zmq_bind(zmq_skt, zmq_bind_addr)) {
-      perror("zmq_bind");
-      cleanup(stream, bar, dev, zmq_ctx, zmq_skt);
-      return -1;
+    perror("zmq_bind");
+    cleanup(&stream, &bar, &dev, &zmq_ctx, &zmq_skt);
+    return -1;
   }
 
   zmq_pollitem_t zmq_pollitem;
@@ -144,8 +146,9 @@ int main(int argc, char *argv[]) {
     dev = new librorc::device(deviceId);
     bar = new librorc::bar(dev, 1);
   } catch (int e) {
-    cerr << "ERROR: failed to initialize C-RORC: " << librorc::errMsg(e) << endl;
-    cleanup(stream, bar, dev, zmq_ctx, zmq_skt);
+    cerr << "ERROR: failed to initialize C-RORC: " << librorc::errMsg(e)
+         << endl;
+    cleanup(&stream, &bar, &dev, &zmq_ctx, &zmq_skt);
     return -1;
   }
 
@@ -154,7 +157,7 @@ int main(int argc, char *argv[]) {
   } catch (int e) {
     cerr << "ERROR: failed to initialize C-RORC: " << librorc::errMsg(e)
          << endl;
-    cleanup(stream, bar, dev, zmq_ctx, zmq_skt);
+    cleanup(&stream, &bar, &dev, &zmq_ctx, &zmq_skt);
     return -1;
   }
 
@@ -162,20 +165,20 @@ int main(int argc, char *argv[]) {
   if (result < 0) {
     cerr << "ERROR: Failed to intialize DMA to Host: "
          << librorc::errMsg(result) << endl;
-    cleanup(stream, bar, dev, zmq_ctx, zmq_skt);
+    cleanup(&stream, &bar, &dev, &zmq_ctx, &zmq_skt);
     return -1;
   }
   result = stream->initializeDmaToDevice(EB_SIZE);
   if (result < 0) {
     cerr << "ERROR: Failed to intialize DMA to Device: "
          << librorc::errMsg(result) << endl;
-    cleanup(stream, bar, dev, zmq_ctx, zmq_skt);
+    cleanup(&stream, &bar, &dev, &zmq_ctx, &zmq_skt);
     return -1;
   }
   if (stream->initializeClusterFinder(mappingfile, tpcPatch, rcuVersion)) {
     cerr << "ERROR: Failed to intialize Clusterfinder with mappingfile "
          << mappingfile << endl;
-    cleanup(stream, bar, dev, zmq_ctx, zmq_skt);
+    cleanup(&stream, &bar, &dev, &zmq_ctx, &zmq_skt);
     return -1;
   }
 
@@ -213,7 +216,7 @@ int main(int argc, char *argv[]) {
     int zmq_ret = zmq_poll(&zmq_pollitem, 1, 0);
     if (zmq_ret < 0) {
       perror("zmq_poll");
-      cleanup(stream, bar, dev, zmq_ctx, zmq_skt);
+      cleanup(&stream, &bar, &dev, &zmq_ctx, &zmq_skt);
       return -1;
     } else if (zmq_ret > 0) {
       char zmq_buffer[ZMQ_BUF_SIZE];
@@ -221,7 +224,7 @@ int main(int argc, char *argv[]) {
       int ret = zmq_recv(zmq_skt, zmq_buffer, ZMQ_BUF_SIZE, 0);
       if (ret == -1) {
         perror("zmq_recv");
-        cleanup(stream, bar, dev, zmq_ctx, zmq_skt);
+        cleanup(&stream, &bar, &dev, &zmq_ctx, &zmq_skt);
         return -1;
       }
       std::string rcvd = std::string(zmq_buffer);
@@ -233,7 +236,7 @@ int main(int argc, char *argv[]) {
       size_t found = rcvd.find_first_of(";");
       if (found == std::string::npos) {
         cerr << "ERROR: Invalid string from ZMQ: %s" << rcvd << endl;
-        cleanup(stream, bar, dev, zmq_ctx, zmq_skt);
+        cleanup(&stream, &bar, &dev, &zmq_ctx, &zmq_skt);
         return -1;
       }
       std::string inputfile = rcvd.substr(0, found);
@@ -241,7 +244,7 @@ int main(int argc, char *argv[]) {
       found = rcvd.find_first_of(";");
       if (found == std::string::npos) {
         cerr << "ERROR: Invalid string from ZMQ: %s" << rcvd << endl;
-        cleanup(stream, bar, dev, zmq_ctx, zmq_skt);
+        cleanup(&stream, &bar, &dev, &zmq_ctx, &zmq_skt);
         return -1;
       }
       std::string outputfile = rcvd.substr(0, found);
@@ -275,8 +278,8 @@ int main(int argc, char *argv[]) {
         delete dev;
         return -1;
       } else if (result == 0) {
-          input_iter = input_file_list.erase(input_iter);
-          cnt_input_done++;
+        input_iter = input_file_list.erase(input_iter);
+        cnt_input_done++;
       }
     }
 
@@ -288,10 +291,11 @@ int main(int argc, char *argv[]) {
       const uint32_t *event = NULL;
       if (stream->pollForEventToHost(&report, &event, &librorcEventReference)) {
         if (output_iter != output_end) {
-            if (event[0] != 0xffffffff) {
-              cout << "WARNING: Invalid CDH in " << *output_iter
-                   << ", file no. " << cnt_output_done << endl;
-            }
+          if (event[0] != 0xffffffff) {
+            cout << "WARNING: Invalid CDH in " << *output_iter << ", file no. "
+                 << cnt_output_done << endl;
+          }
+          checkHwcfFlags(report, output_iter->c_str());
           eventToDisk(output_iter->c_str(), report, event);
           output_iter = output_file_list.erase(output_iter);
           cnt_output_done++;
@@ -320,16 +324,13 @@ int main(int argc, char *argv[]) {
            << ", Input Queue: " << input_file_list.size()
            << ", Input done: " << cnt_input_done
            << ", Output done: " << cnt_output_done << endl;
-        last = now;
+      last = now;
     }
   }
 
-  cleanup(stream, bar, dev, zmq_ctx, zmq_skt);
+  cleanup(&stream, &bar, &dev, &zmq_ctx, &zmq_skt);
   return 0;
 }
-
-
-
 
 #if 0
 int checkEvent(librorc::EventDescriptor *report, const uint32_t *event) {
@@ -341,9 +342,21 @@ int checkEvent(librorc::EventDescriptor *report, const uint32_t *event) {
 };
 #endif
 
+void checkHwcfFlags(librorc::EventDescriptor *report,
+                    const char *outputFileName) {
+  uint32_t hwcfflags = (report->calc_event_size >> 30) & 0x3;
+  if (hwcfflags & 0x1) {
+    printf("WARNING: Found RCU protocol error(s) in %s\n", outputFileName);
+  }
+  if (hwcfflags & 0x2) {
+    printf("WARNING: Found ALTRO channel error flag(s) set in %s\n",
+           outputFileName);
+  }
+}
+
 int eventToDisk(const char *filename, librorc::EventDescriptor *report,
                 const uint32_t *event) {
-  int fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR|S_IWUSR);
+  int fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
   if (fd < 0) {
     cerr << "ERROR: Failed to open output file " << filename << ": "
          << strerror(errno) << endl;
@@ -386,7 +399,7 @@ int compareToRef(const char *filename, librorc::EventDescriptor *report,
     return -1;
   }
   uint32_t dmaWords = (report->calc_event_size & 0x3fffffff);
-  ssize_t eventSize = (dmaWords<<2);
+  ssize_t eventSize = (dmaWords << 2);
 
   if (fd_stat.st_size != eventSize) {
     cerr << "Event Size mismatch, got " << eventSize << " bytes, expected "
@@ -405,27 +418,27 @@ int compareToRef(const char *filename, librorc::EventDescriptor *report,
   return 0;
 }
 
-void cleanup(crorc_hwcf_coproc_handler * stream, librorc::bar * bar,
-             librorc::device * dev, void * zmq_ctx, void * zmq_skt) {
-  if (stream) {
-    delete stream;
-    stream = NULL;
+void cleanup(crorc_hwcf_coproc_handler **stream, librorc::bar **bar,
+             librorc::device **dev, void **zmq_ctx, void **zmq_skt) {
+  if (*stream) {
+    delete *stream;
+    *stream = NULL;
   }
-  if (bar) {
-    delete bar;
-    bar = NULL;
+  if (*bar) {
+    delete *bar;
+    *bar = NULL;
   }
-  if (dev) {
-    delete dev;
-    dev = NULL;
+  if (*dev) {
+    delete *dev;
+    *dev = NULL;
   }
-  if (zmq_skt) {
-    zmq_close(zmq_skt);
-    zmq_skt = NULL;
+  if (*zmq_skt) {
+    zmq_close(*zmq_skt);
+    *zmq_skt = NULL;
   }
-  if (zmq_ctx) {
-    zmq_term(zmq_ctx);
-    zmq_ctx = NULL;
+  if (*zmq_ctx) {
+    zmq_term(*zmq_ctx);
+    *zmq_ctx = NULL;
   }
 }
 
