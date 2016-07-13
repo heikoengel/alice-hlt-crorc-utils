58b137f6084e720e9ecd440b49ddd15f187a0b16
Heiko Engel <hengel@cern.ch>
2016-07-13 10:25:48 +0200

    crorc_dma_benchmark: additional options for sweeps

--- src/crorc_dma_benchmark.cpp
+++ src/crorc_dma_benchmark.cpp
@@ -27,6 +27,17 @@ using namespace std;
 #define DEVICE_ID 0
 #define EVENTBUFFER_SIZE (1<<30)
 
+#define PG_EVENTSIZE_DW_START 64
+#define PG_EVENTSIZE_DW_END   0x800000
+#define SAMPLE_TIME        1.0
+#define SWEEP_SAMPLES      5
+
+#define HELP_TEXT                                                              \
+  "crorc_dma_benchmark options: "                                              \
+  "   -s [size]      PCIe packet size in bytes, default: max. supported value" \
+  "   -p [size]      PatternGenerator event size in DWs or 0 for PRBS size"    \
+  "   -S             Sweep PatternGenerator event size"
+
 bool done = false;
 void abort_handler(int s) {
   cerr << "Caught signal " << s << endl;
@@ -37,10 +48,56 @@ void abort_handler(int s) {
   }
 }
 
+uint32_t getNextEventSize(uint32_t eventSizeDw, uint32_t maxPayloadBytes) {
+  uint32_t numPkts = (eventSizeDw << 2) / maxPayloadBytes;
+  uint32_t nextSize = ((numPkts + 1) * maxPayloadBytes) >> 2;
+  if (nextSize > PG_EVENTSIZE_DW_END) {
+    return PG_EVENTSIZE_DW_START;
+  } else {
+    return nextSize;
+  }
+}
+
 int main(int argc, char *argv[]) {
 
   librorc::event_stream *es[12];
   librorc::patterngenerator *pg[12];
+
+  int arg;
+  uint32_t pgSizeDw = 0;
+  int pcieSize = 0;
+  int pgSweep = 0;
+  uint32_t maxPayloadBytes = 0;
+
+  while ((arg = getopt(argc, argv, "s:p:Sh")) != -1) {
+    switch (arg) {
+    case 'h':
+      cout << HELP_TEXT;
+      return 0;
+    case 's':
+      pcieSize = strtol(optarg, NULL, 0);
+      if (pcieSize < 0 || pcieSize > 256) {
+        cerr << "ERROR: invalid PCIe payload size: " << pcieSize << endl;
+        return -1;
+      }
+      break;
+    case 'p':
+      pgSizeDw = strtoul(optarg, NULL, 0);
+      if (pgSizeDw < 0) {
+        cerr << "ERROR: invalid PG event size: " << pgSizeDw << endl;
+        return -1;
+      }
+      break;
+    case 'S':
+      pgSweep = 1;
+      pgSizeDw = PG_EVENTSIZE_DW_START;
+      break;
+    default:
+      cerr << "ERROR: Unknown parameter " << arg << endl;
+      cout << HELP_TEXT;
+      return -1;
+    }
+  }
 
   int max_channels = 12;
   int i;
@@ -59,8 +116,14 @@ int main(int argc, char *argv[]) {
       }
       pg[i]->disable();
       pg[i]->configureMode(PG_PATTERN_INC, 0, 0);
-      pg[i]->setPrbsSize(0x100, 0x3f0000);
-      //es[i]->overridePciePacketSize(128);
+      if (pgSizeDw) {
+        pg[i]->setStaticEventSize(pgSizeDw);
+      } else {
+        pg[i]->setPrbsSize(0x100, 0x3f0000);
+      }
+      if (pcieSize) {
+        es[i]->overridePciePacketSize(pcieSize);
+      }
       es[i]->initializeDma(2*i, EVENTBUFFER_SIZE);
       es[i]->m_link->setChannelActive(0);
       es[i]->m_link->setFlowControlEnable(1);
@@ -87,6 +150,12 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
+  if (pcieSize) {
+    maxPayloadBytes = pcieSize;
+  } else {
+    maxPayloadBytes = es[0]->m_dev->maxPayloadSize();
+  }
+
   es[0]->m_sm->clearMaxPcieDeadtime();
   cout << "Starting PatternGenerators..." << endl;
   for (i = 0; i < max_channels; i++) {
@@ -106,8 +175,14 @@ int main(int argc, char *argv[]) {
   struct timeval tNow, tLast;
   gettimeofday(&tNow, NULL);
   tLast = tNow;
-  uint64_t totalBytes = 0;
-  uint64_t lastBytes = 0;
+  uint64_t ebBytes = 0;
+  uint64_t ebLastBytes = 0;
+  uint64_t rbBytes = 0;
+  uint64_t rbLastBytes = 0;
+  int nSamples = 0;
+  int pgSizeDwChanged = 0;
+        
+  cout << "# pgSizeDw, ebRate, rbRate, deadtime" << endl;
 
   while (!done) {
     gettimeofday(&tNow, NULL);
@@ -115,23 +190,47 @@ int main(int argc, char *argv[]) {
       if (es[i]->getNextEvent(&report, &event, &librorcReference)) {
         es[i]->updateChannelStatus(report);
         uint32_t bytesize = (report->calc_event_size & 0x3fffffff) << 2;
-        totalBytes += bytesize;
+        ebBytes += bytesize + 4; //EOE-Word
+        rbBytes += sizeof(librorc::EventDescriptor);
         es[i]->releaseEvent(librorcReference);
       }
     }
     double tdiff = librorc::gettimeofdayDiff(tLast, tNow);
-    if (tdiff > 1.0) {
+    if (tdiff >= SAMPLE_TIME) {
       float deadtime = es[0]->m_sm->maxPcieDeadtime();
       es[0]->m_sm->clearMaxPcieDeadtime();
-      uint64_t bytesDiff = totalBytes - lastBytes;
-      double totalRate = ((bytesDiff/tdiff)/1000.0/1000.0);
-      cout << totalRate << " MB/s, " << totalRate / max_channels
+      uint64_t ebBytesDiff = ebBytes - ebLastBytes;
+      uint64_t rbBytesDiff = rbBytes - rbLastBytes;
+      double ebRate = ((ebBytesDiff/tdiff)/1000.0/1000.0);
+      double rbRate = ((rbBytesDiff/tdiff)/1000.0/1000.0);
+      /*cout << ebRate << " MB/s, " << ebRate / max_channels
            << " MB/s per channel, max. Deadtime: " << deadtime
-           << " us" << endl;
-      lastBytes = totalBytes;
+           << " us" << endl;*/
+      if(!pgSizeDwChanged) {
+        cout << pgSizeDw << ", " << ebRate << ", " << rbRate << ", " << deadtime
+             << endl;
+      } else {
+        pgSizeDwChanged = 0;
+      }
+
+      ebLastBytes = ebBytes;
+      rbLastBytes = rbBytes;
       tLast = tNow;
-    }
-  }
+
+      if (pgSweep) {
+        if (nSamples < SWEEP_SAMPLES) {
+          nSamples++;
+        } else {
+          nSamples = 0;
+          pgSizeDw = getNextEventSize(pgSizeDw, maxPayloadBytes);
+          for (i = 0; i < max_channels; i++) {
+            pg[i]->setStaticEventSize(pgSizeDw-1); //EOE-Word
+          }
+          pgSizeDwChanged = 1; // skip one sample after change
+        }
+      } // pgSweep
+    } // tdiff
+  } // while(!done)
 
   for (i = 0; i < max_channels; i++) {
     if (pg[i]) {
