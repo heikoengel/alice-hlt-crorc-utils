844eefcac79cc34479bf0c98fe9109767bf2300b
Heiko Engel <hengel@cern.ch>
2016-07-27 14:31:48 +0200

    crorc_dma_benchmark: using threads

--- src/crorc_dma_benchmark.cpp
+++ src/crorc_dma_benchmark.cpp
@@ -19,6 +19,7 @@
  * */
 
 #include <iostream>
+#include <thread>
 #include <librorc.h>
 #include <signal.h>
 
@@ -29,14 +30,16 @@ using namespace std;
 
 #define PG_EVENTSIZE_DW_START 64
 #define PG_EVENTSIZE_DW_END   0x800000
-#define SAMPLE_TIME        1.0
-#define SWEEP_SAMPLES      5
+#define SAMPLE_TIME           1.0
+#define SWEEP_SAMPLES         5
+#define THREAD_USLEEP         1000
 
 #define HELP_TEXT                                                              \
   "crorc_dma_benchmark options: "                                              \
   "   -s [size]      PCIe packet size in bytes, default: max. supported value" \
   "   -p [size]      PatternGenerator event size in DWs or 0 for PRBS size"    \
-  "   -S             Sweep PatternGenerator event size"
+  "   -S             Sweep PatternGenerator event size"                        \
+  "   -v             Verbose mode"
 
 bool done = false;
 void abort_handler(int s) {
@@ -48,9 +51,17 @@ void abort_handler(int s) {
   }
 }
 
+struct rdoInfo_t {
+  uint64_t ebBytes;
+  uint64_t rbBytes;
+  uint32_t lastEventSizeDw;
+};
+
+
 uint32_t getNextEventSize(uint32_t eventSizeDw, uint32_t maxPayloadBytes) {
   uint32_t numPkts = (eventSizeDw << 2) / maxPayloadBytes;
   uint32_t nextSize = ((numPkts + 1) * maxPayloadBytes) >> 2;
+  cout << "Setting ES=" << nextSize << endl;
   if (nextSize > PG_EVENTSIZE_DW_END) {
     return PG_EVENTSIZE_DW_START;
   } else {
@@ -58,18 +69,89 @@ uint32_t getNextEventSize(uint32_t eventSizeDw, uint32_t maxPayloadBytes) {
   }
 }
 
+void ChannelReadout(int chId) { //, struct rdoInfo_t &rdoInfo) {
+
+  librorc::event_stream *es = NULL;
+  librorc::patterngenerator *pg = NULL;
+
+  try {
+    es =
+        new librorc::event_stream(DEVICE_ID, chId, librorc::kEventStreamToHost);
+    pg = es->getPatternGenerator();
+    if (pg == NULL) {
+      throw(-1);
+    }
+    pg->disable();
+    pg->configureMode(PG_PATTERN_INC, 0, 0);
+    //if (pgSizeDw) {
+    pg->setStaticEventSize(PG_EVENTSIZE_DW_START);
+    //} else {
+    //  pg->setPrbsSize(0x100, 0x3f0000);
+    //}
+    /*if (pcieSize) {
+      es->overridePciePacketSize(pcieSize);
+    }*/
+    es->initializeDma(2 * chId, EVENTBUFFER_SIZE);
+    es->m_link->setChannelActive(0);
+    es->m_link->setFlowControlEnable(1);
+    es->m_channel->clearEventCount();
+    es->m_channel->clearStallCount();
+    es->m_channel->readAndClearPtrStallFlags();
+    es->m_link->setDataSourcePatternGenerator();
+    es->m_link->setChannelActive(1);
+    pg->enable();
+    cout << "Ch" << chId << " init done." << endl;
+  }
+  catch (int e) {
+    cout << "Channel " << chId
+         << " failed event stream initialization: " << librorc::errMsg(e)
+         << endl;
+    if (es) {
+      delete es;
+      es = NULL;
+    }
+  }
+
+  while (!done) {
+    librorc::EventDescriptor *report = NULL;
+    const uint32_t *event = NULL;
+    uint64_t librorcReference;
+    if (es->getNextEvent(&report, &event, &librorcReference)) {
+      //es->updateChannelStatus(report);
+#if 0
+      uint32_t bytesize = (report->calc_event_size & 0x3fffffff) << 2;
+      rdoInfo.ebBytes += (bytesize + 4); //EOE-Word
+      rdoInfo.rbBytes += sizeof(librorc::EventDescriptor);
+      rdoInfo.lastEventSizeDw = bytesize >> 2;
+#endif
+      es->releaseEvent(librorcReference);
+    }/* else {
+      usleep(THREAD_USLEEP);
+    }*/
+  }
+  if (pg) {
+    pg->disable();
+    delete pg;
+  }
+  if (es) {
+    es->m_link->setFlowControlEnable(0);
+    es->m_link->setChannelActive(0);
+    delete es;
+  }
+    cout << "Ch" << chId << " de-init done." << endl;
+}
+
 int main(int argc, char *argv[]) {
 
-  librorc::event_stream *es[12];
-  librorc::patterngenerator *pg[12];
 
   int arg;
   uint32_t pgSizeDw = 0;
   int pcieSize = 0;
   int pgSweep = 0;
   uint32_t maxPayloadBytes = 0;
+  int verbose = 0;
 
-  while ((arg = getopt(argc, argv, "s:p:Sh")) != -1) {
+  while ((arg = getopt(argc, argv, "s:p:Svh")) != -1) {
     switch (arg) {
     case 'h':
       cout << HELP_TEXT;
@@ -92,6 +174,9 @@ int main(int argc, char *argv[]) {
       pgSweep = 1;
       pgSizeDw = PG_EVENTSIZE_DW_START;
       break;
+    case 'v':
+      verbose = 1;
+      break;
     default:
       cerr << "ERROR: Unknown parameter " << arg << endl;
       cout << HELP_TEXT;
@@ -100,67 +185,6 @@ int main(int argc, char *argv[]) {
   }
 
   int max_channels = 12;
-  int i;
-
-  for (i = 0; i < max_channels; i++) {
-    es[i] = NULL;
-    pg[i] = NULL;
-  }
-  try {
-    for (i = 0; i < max_channels; i++) {
-      cout << "Initializing Channel " << i << endl;
-      es[i] = new librorc::event_stream(DEVICE_ID, i, librorc::kEventStreamToHost);
-      pg[i] = es[i]->getPatternGenerator();
-      if(pg[i] == NULL) {
-        throw(-1);
-      }
-      pg[i]->disable();
-      pg[i]->configureMode(PG_PATTERN_INC, 0, 0);
-      if (pgSizeDw) {
-        pg[i]->setStaticEventSize(pgSizeDw);
-      } else {
-        pg[i]->setPrbsSize(0x100, 0x3f0000);
-      }
-      if (pcieSize) {
-        es[i]->overridePciePacketSize(pcieSize);
-      }
-      es[i]->initializeDma(2*i, EVENTBUFFER_SIZE);
-      es[i]->m_link->setChannelActive(0);
-      es[i]->m_link->setFlowControlEnable(1);
-      es[i]->m_channel->clearEventCount();
-      es[i]->m_channel->clearStallCount();
-      es[i]->m_channel->readAndClearPtrStallFlags();
-      es[i]->m_link->setDataSourcePatternGenerator();
-      es[i]->m_link->setChannelActive(1);
-    }
-  }
-  catch (int e) {
-    cout << "Channel " << i
-         << " failed event stream initialization: " << librorc::errMsg(e)
-         << endl << "Skipping this channel and all following." << endl;
-    max_channels = i + 1;
-    if (es[i]) {
-      delete es[i];
-      es[i] = NULL;
-    }
-  }
-
-  if (max_channels == 1) {
-    cerr << "ERROR: No channel could be intialized - exiting!" << endl;
-    return -1;
-  }
-
-  if (pcieSize) {
-    maxPayloadBytes = pcieSize;
-  } else {
-    maxPayloadBytes = es[0]->m_dev->maxPayloadSize();
-  }
-
-  es[0]->m_sm->clearMaxPcieDeadtime();
-  cout << "Starting PatternGenerators..." << endl;
-  for (i = 0; i < max_channels; i++) {
-    pg[i]->enable();
-  }
 
   struct sigaction sigIntHandler;
   sigIntHandler.sa_handler = abort_handler;
@@ -168,56 +192,47 @@ int main(int argc, char *argv[]) {
   sigIntHandler.sa_flags = 0;
   sigaction(SIGINT, &sigIntHandler, NULL);
 
-  librorc::EventDescriptor *report = NULL;
-  const uint32_t *event = NULL;
-  uint64_t librorcReference;
-
   struct timeval tNow, tLast;
   gettimeofday(&tNow, NULL);
   tLast = tNow;
-  uint64_t ebBytes = 0;
   uint64_t ebLastBytes = 0;
-  uint64_t rbBytes = 0;
   uint64_t rbLastBytes = 0;
   int nSamples = 0;
-  int pgSizeDwChanged = 0;
-        
-  cout << "# pgSizeDw, ebRate, rbRate, deadtime" << endl;
 
+  std::thread rdoThread[max_channels];
+  //struct rdoInfo_t rdoInfo[max_channels];
+
+  cout << "# pgSizeDw, ebRate, rbRate" << endl;
+    
+  for (int i = 0; i < max_channels; i++) {
+    //rdoInfo[i].ebBytes = 0;
+    //rdoInfo[i].rbBytes = 0;
+    rdoThread[i] = std::thread(ChannelReadout, i);//, std::ref(rdoInfo[i]));
+  }
+
+
+#if 0
   while (!done) {
     gettimeofday(&tNow, NULL);
-    for (i = 0; i < max_channels; i++) {
-      if (es[i]->getNextEvent(&report, &event, &librorcReference)) {
-        es[i]->updateChannelStatus(report);
-        uint32_t bytesize = (report->calc_event_size & 0x3fffffff) << 2;
-        ebBytes += bytesize + 4; //EOE-Word
-        rbBytes += sizeof(librorc::EventDescriptor);
-        es[i]->releaseEvent(librorcReference);
-      }
-    }
     double tdiff = librorc::gettimeofdayDiff(tLast, tNow);
     if (tdiff >= SAMPLE_TIME) {
-      float deadtime = es[0]->m_sm->maxPcieDeadtime();
-      es[0]->m_sm->clearMaxPcieDeadtime();
+      uint64_t ebBytes = 0;
+      uint64_t rbBytes = 0;
+      for (int i = 0; i < max_channels; i++) {
+        ebBytes += rdoInfo[i].ebBytes;
+        rbBytes += rdoInfo[i].rbBytes;
+      }
       uint64_t ebBytesDiff = ebBytes - ebLastBytes;
       uint64_t rbBytesDiff = rbBytes - rbLastBytes;
       double ebRate = ((ebBytesDiff/tdiff)/1000.0/1000.0);
       double rbRate = ((rbBytesDiff/tdiff)/1000.0/1000.0);
-      /*cout << ebRate << " MB/s, " << ebRate / max_channels
-           << " MB/s per channel, max. Deadtime: " << deadtime
-           << " us" << endl;*/
-      if(!pgSizeDwChanged) {
-        cout << pgSizeDw << ", " << ebRate << ", " << rbRate << ", " << deadtime
-             << endl;
-      } else {
-        pgSizeDwChanged = 0;
-      }
+        cout << pgSizeDw << ", " << ebRate << ", " << rbRate << endl;
 
       ebLastBytes = ebBytes;
       rbLastBytes = rbBytes;
       tLast = tNow;
 
-      if (pgSweep) {
+      /*if (pgSweep) {
         if (nSamples < SWEEP_SAMPLES) {
           nSamples++;
         } else {
@@ -226,22 +241,15 @@ int main(int argc, char *argv[]) {
           for (i = 0; i < max_channels; i++) {
             pg[i]->setStaticEventSize(pgSizeDw-1); //EOE-Word
           }
-          pgSizeDwChanged = 1; // skip one sample after change
+          //pgSizeDwChanged = 1; // skip one sample after change
         }
-      } // pgSweep
+      } // pgSweep*/
     } // tdiff
   } // while(!done)
+#endif
 
-  for (i = 0; i < max_channels; i++) {
-    if (pg[i]) {
-      pg[i]->disable();
-      delete pg[i];
-    }
-    if (es[i]) {
-      es[i]->m_link->setFlowControlEnable(0);
-      es[i]->m_link->setChannelActive(0);
-      delete es[i];
-    }
+  for (int i = 0; i < max_channels; i++) {
+    rdoThread[i].join();
   }
 
   return 0;
